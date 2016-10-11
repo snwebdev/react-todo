fc3dcba464c20b2937d6b669b5fa5a3de9af75b0
stewart <stewart@snwebdev.com>
2016-10-11 21:10:45 +0100

    add Todo module

--- app/app.jsx
+++ app/app.jsx
@@ -2,6 +2,8 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 
+var Todo = require('Todo');
+
 //load foundation
 $(document).foundation();
 
@@ -11,6 +13,6 @@ require('style!css!sass!applicationStyles');
 
 
 ReactDOM.render(
-  <p>Boilerplate 3 Project</p>,
+  <Todo/>,
   document.getElementById("app")
 );
