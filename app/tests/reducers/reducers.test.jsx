8d9b9ff887c0e94bb07307a7e410c0032b875e93
stewart <stewart@snwebdev.com>
2016-10-18 12:39:07 +0100

    todos reducer and tests

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -1,29 +1,60 @@
 var expect = require('expect');
-var reducers = require('reducers');
 var df = require('deep-freeze-strict');
 
-describe('Reducers', () => {
+var reducers = require('reducers');
 
+describe('Reducers', () => {
   describe('searchTextReducer', () => {
     it('should set searchText', () => {
       var action = {
         type: 'SET_SEARCH_TEXT',
-        searchText: 'mince'
+        searchText: 'dog'
       };
-      var res = reducers.searchTextReducer(df(''), df(action))
+      var res = reducers.searchTextReducer(df(''), df(action));
 
       expect(res).toEqual(action.searchText);
     });
   });
 
-  describe('ShowCompletedReducer', () => {
-    it('should flip showCompleted status', () => {
+  describe('showCompletedReducer', () => {
+    it('should toggle showCompleted', () => {
       var action = {
-        type: 'TOGGLE_SHOW_COMPLTED',
+        type: 'TOGGLE_SHOW_COMPLETED'
       };
-      var res = reducers.showCompletedReducer(df(false), df(action))
+      var res = reducers.showCompletedReducer(df(false), df(action));
 
       expect(res).toEqual(true);
+    });
+  });
+
+  describe('todosReducer', () => {
+    it('should add new todo', () => {
+      var action = {
+        type: 'ADD_TODO',
+        text: 'Walk the dog'
+      };
+      var res = reducers.todosReducer(df([]), df(action));
+
+      expect(res.length).toEqual(1);
+      expect(res[0].text).toEqual(action.text);
+    });
+
+    it('should toggle todo', () => {
+      var todos = [{
+        id: '123',
+        text: 'Something',
+        completed: true,
+        createdAt: 123,
+        completedAt: 125
+      }];
+      var action = {
+        type: 'TOGGLE_TODO',
+        id: '123'
+      };
+      var res = reducers.todosReducer(df(todos), df(action));
+
+      expect(res[0].completed).toEqual(false);
+      expect(res[0].completedAt).toEqual(undefined);
     });
   });
 });
